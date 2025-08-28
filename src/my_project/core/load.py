1f36f5adaf3d7043f28c86a77fc9a2c28d65c8fc
Anton Miasoedov <anton.myasoedov@gmail.com>
2025-08-27 23:11:30 -0400

    changed database to local instead of passing url

--- src/my_project/core/load.py
+++ src/my_project/core/load.py
@@ -4,21 +4,40 @@ import pandas as pd
 from sqlalchemy import create_engine
 from sqlalchemy.engine import Engine
 
-def make_engine(db_url: str) -> Engine:
-    return create_engine(db_url, future=True)
+def make_engine(db_name: str) -> Engine:
+
+
+    connection_url = (
+        f"mssql+pyodbc://@localhost/test"
+        "?driver=ODBC+Driver+17+for+SQL+Server"
+        "&trusted_connection=yes"
+    )
+    return create_engine(connection_url, future=True)
 
 def to_sql(
     df: pd.DataFrame,
     engine: Engine,
     table: str,
-    if_exists: str = "append",
+    if_exists: str = "append",  # "append" | "replace" | "fail"
     chunksize: Optional[int] = None,
 ) -> None:
+
     df.to_sql(
         table,
         con=engine,
-        if_exists=if_exists,   # "append" | "replace" | "fail"
+        if_exists=if_exists,
         index=False,
         chunksize=chunksize,
         method=None,
     )
+
+
+if __name__ == "__main__":
+
+    df = pd.DataFrame({"id": [1, 2], "name": ["Alice", "Bob"]})
+
+
+    engine = make_engine("Test")
+
+    # Load DataFrame into table "my_table"
+    to_sql(df, engine, "planets_pscomppars")
